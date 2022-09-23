ffaea7aaaedf0489d39aa11fec783a5921d1ffba
qwersteve07 <qwersteve07@gmail.com>
2022-09-23 22:54:33 +0800

    wip

--- src/App.js
+++ src/App.js
@@ -2,72 +2,73 @@ import styles from './app.module.sass';
 import random from 'lodash/random';
 import { useEffect, useRef, useState } from 'react';
 import classnames from 'classnames/bind';
-import firebase from 'firebase/app';
-import 'firebase/storage';
 import ReactLoading from 'react-loading';
-import iconRefresh from './images/refresh.svg';
-const cx = classnames.bind(styles);
+import iconRefresh from './assets/refresh.svg';
+import iconLogo from './assets/logo.svg';
 
-var firebaseConfig = {
-  apiKey: 'AIzaSyCHjUOVqUBjEXJyRgxosyBgQJmtEGEAg1A',
-  authDomain: 'unme-quotation.firebaseapp.com',
-  projectId: 'unme-quotation',
-  storageBucket: 'unme-quotation.appspot.com',
-  messagingSenderId: '508520909491',
-  appId: '1:508520909491:web:bab6e4e495395f0e50fec6',
-  measurementId: 'G-S5H2Q2P1NV',
-};
-// Initialize Firebase
-firebase.initializeApp(firebaseConfig);
+import CircularProgress from '@mui/material/CircularProgress';
+import Box from '@mui/material/Box';
+import Button from '@mui/material/Button';
+import TextField from '@mui/material/TextField';
+import Dialog from '@mui/material/Dialog';
+import DialogActions from '@mui/material/DialogActions';
+import DialogContent from '@mui/material/DialogContent';
+import DialogTitle from '@mui/material/DialogTitle';
+const cx = classnames.bind(styles);
 
 function App() {
   const inputRef = useRef('');
   const [imageUrl, setImageUrl] = useState();
   const [imageLoad, setImageLoad] = useState(false);
   const [uploadState, setUploadState] = useState('init');
-  const storage = firebase.storage();
+  const [dialogShow, setDialogShow] = useState(false);
 
   const fetchImage = () => {
-    var listRef = storage.ref().child('');
-
-    listRef
-      .listAll()
-      .then(res => {
-        // get item
-        const length = res.items.length;
-
-        const pickItem = res.items[random(0, length - 1)];
-        // get item url
-        storage
-          .ref(pickItem.name)
-          .getDownloadURL()
-          .then(url => {
-            setImageUrl(url);
-          });
-      })
-      .catch(error => {
-        // Uh-oh, an error occurred!
-      });
+    // var listRef = storage.ref().child('');
+    // listRef
+    //   .listAll()
+    //   .then(res => {
+    //     // get item
+    //     const length = res.items.length;
+    //     const pickItem = res.items[random(0, length - 1)];
+    //     // get item url
+    //     storage
+    //       .ref(pickItem.name)
+    //       .getDownloadURL()
+    //       .then(url => {
+    //         setImageUrl(url);
+    //       });
+    //   })
+    //   .catch(error => {
+    //     // Uh-oh, an error occurred!
+    //   });
   };
 
   useEffect(() => {
     fetchImage();
   }, []); // eslint-disable-line
 
-  const uploadFile = file => {
+  const uploadFile = async file => {
     setUploadState('uploading');
-    const ref = storage.ref();
-    const name = file.name;
-    const metadata = {
-      contentType: file.type,
-    };
-    const task = ref.child(name).put(file, metadata);
-    task
-      .then(snapshot => snapshot.ref.getDownloadURL())
-      .then(url => {
-        setUploadState('done');
+
+    // const data = {
+    //   image: file,
+    // };
+
+    fetch('http://localhost:8080/images', {
+      body: {
+        imageUrl,
+      },
+      method: 'POST',
+    })
+      .then(data => {
+        console.log(data);
       })
-      .catch(e => console.error(e));
+      .catch(err => {
+        console.log(err);
+      });
+
+    setUploadState('done');
   };
 
   const refreshImage = () => {
@@ -83,8 +84,24 @@ function App() {
     return <></>;
   };
 
+  const handleDialogOpen = () => setDialogShow(true);
+  const handleDialogClose = () => setDialogShow(false);
+  const handleUploadFile = async file => {
+    let data = new FormData();
+    data.append('file', file);
+    const imageUrl = await fetch('http://localhost:8080/upload', {
+      body: data,
+      method: 'POST',
+    })
+      .then(res => res.json())
+      .then(value => console.log(value))
+      .catch(err => console.log(err));
+  };
+  const handleContribute = () => {};
+
   return (
     <div className={styles.app}>
+      <img src={iconLogo} alt="logo" width="70" />
       <h1>UNME 陪你過日子</h1>
       <p>就讓非我設計的每個人，在你最失意的時刻，用最真實的模樣陪伴著你。</p>
       <div className={cx({ container: true, load: imageLoad })}>
@@ -92,12 +109,26 @@ function App() {
           <img src={iconRefresh} alt="refresh" />
         </div>
         <div className={styles.image}>
-          {(!imageUrl || !imageLoad) && <ReactLoading type="spin" color="#999999" height={20} width={20} />}
+          {(!imageUrl || !imageLoad) && (
+            <Box sx={{ display: 'flex' }}>
+              <CircularProgress />
+            </Box>
+          )}
           <img src={imageUrl} alt="quote" onLoad={() => setImageLoad(true)} />
         </div>
       </div>
+
+      <Button variant="outlined" onClick={handleDialogOpen}>
+        上傳 UNME 的歡樂時光
+      </Button>
+      <ContributeDialog
+        show={dialogShow}
+        onDismiss={handleDialogClose}
+        onUploadFile={handleUploadFile}
+        onContribute={handleContribute}
+      />
+      {/* 
       <div className={styles.contribute}>
-        你也想讓我們的每一天能夠更快樂嗎？
         <input
           type="file"
           id="file"
@@ -119,7 +150,7 @@ function App() {
         <div className={styles.thanks}>
           <Thanks />
         </div>
-      </div>
+      </div> */}
       <footer>
         Design by Steve Lee .<br />
         Contribution by UNME Design.
@@ -127,5 +158,55 @@ function App() {
     </div>
   );
 }
+
+const ContributeDialog = ({ show, onDismiss, onUploadFile, onContribute }) => {
+  return (
+    <Dialog open={show} onClose={onDismiss}>
+      <DialogTitle>DO IT</DialogTitle>
+      <DialogContent>
+        <TextField
+          autoFocus
+          margin="dense"
+          id="name"
+          label="名稱"
+          placeholder="是哪位動物熱心的提供呢？"
+          type="text"
+          fullWidth
+          variant="standard"
+        />
+        <TextField
+          autoFocus
+          margin="dense"
+          id="comment"
+          label="說明"
+          placeholder="請補充這人在幹嘛"
+          type="text"
+          fullWidth
+          variant="standard"
+        />
+
+        <Button margin="dense" variant="outlined" component="label">
+          選擇圖片
+          <input
+            hidden
+            accept="image/*"
+            type="file"
+            onChange={e => {
+              onUploadFile(e.target.files[0]);
+            }}
+          />
+        </Button>
+      </DialogContent>
+      <DialogActions>
+        <Button variant="text" onClick={onDismiss}>
+          取消
+        </Button>
+        <Button variant="contained" onClick={onContribute}>
+          傳送！
+        </Button>
+      </DialogActions>
+    </Dialog>
+  );
+};
 
 export default App;
